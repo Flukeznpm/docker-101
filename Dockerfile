398ae94d4d18e1c13f98ddfa517c7cc5dc77b44d
Flukeznpm <nattapong.meun@gmail.com>
2023-01-21 20:52:03 +0700

    [EDIT] port docker

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM node:14-alpine as backend
 RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
-COPY package.json /usr/src/app/
+COPY package*.json /usr/src/app/
 RUN npm install
 COPY . /usr/src/app
 EXPOSE 8000
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 8000;
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const userRouter = require("./routes/user/index");
